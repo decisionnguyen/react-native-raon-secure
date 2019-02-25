a3f2fe9303f9f3162e4e515036287b4f91986b11
Suhan Moon <lunyx@me.com>
2019-02-25 09:22:50 +0900

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-
-import { NativeModules } from 'react-native';
+import React from "react";
+import {ActivityIndicator, NativeModules, StyleSheet, Text, View,} from 'react-native';
 
 const { RNRaonSecure } = NativeModules;
 
-export default RNRaonSecure;
+export default class RaonSecure extends React.Component {
+
+    state = {};
+
+    constructor(props){
+        super(props);
+    }
+
+    render () {
+
+        const { labelStyle, textStyle, buttonStyle, disableButtonStyle, ...props } = this.props;
+
+        return (
+            <View style={ styles.container }>
+                <Text> RaonSecure </Text>
+                <ActivityIndicator />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        alignItems: 'center',
+    }
+});
+
